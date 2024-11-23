874112fa4dd8942a4f79bf6da69bfc736a4d49a6
hajratariqi <hajra.ashrafqadri@gmail.com>
2024-11-23 22:12:22 +0500

    practice complex called callback hell function

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const getData = (id, nextFunc) =>{
-    setTimeout(()=>{
-        console.log(id)
-       if(nextFunc){
-        nextFunc()
-       }
-    }, 1000)
-}
-getData(1, function() {
-    getData(10)
+    setTimeout(() => {
+    console.log(id);
+    if(nextFunc){
+    nextFunc()
+    }
+}, 2000)
+}  
+
+// this is called callback hell
+getData(1, () => {
+    console.log('Added 1...');
+    getData(10, () => {
+    console.log('Added 10...');
+        getData(100, () => {
+    console.log('Added 100...');
+            getData(1000, () => {
+        console.log('Added 1000...');
+            })
+        })
+    })
 })
