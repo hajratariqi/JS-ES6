ed0d9767fcbe472ac3b3309474ff96bd26cd79b8
hajratariqi <hajra.ashrafqadri@gmail.com>
2024-11-23 22:14:21 +0500

    callback hell practice done

--- app.js
+++ app.js
@@ -48,3 +48,51 @@ const bakePizza = () =>{
     }, 2000)
 }
 makePizza(makeDought)
+
+
+
+// const getData = (id, nextFunc) =>{
+// //     setTimeout(() => {
+// //     console.log(id);
+// //     if(nextFunc){
+// //     nextFunc()
+// //     }
+// // }, 2000)
+// // }
+
+// // // this is called callback hell
+// // getData(1, () => {
+// //     console.log('Added 1...');
+// //     getData(10, () => {
+// //     console.log('Added 10...');
+// //         getData(100, () => {
+// //     console.log('Added 100...');
+// //             getData(1000, () => {
+// //         console.log('Added 1000...');
+// //             })
+// //         })
+// //     })
+// // })
+
+// // understanding of promise concept
+
+// let isPizza = true;
+
+// const makePizza = (makeDoughtFunc) => {
+//   if (isPizza) {
+//     setTimeout(() => {
+//       console.log("Cheese is here...");
+//       makeDoughtFunc();
+//     }, 2000);
+//   } else {
+//     console.log("SOrry pizza is not found");
+//   }
+// };
+
+// makePizza(
+//     () => {
+//   setTimeout(() => {
+//     console.log("pizza dought is ready");
+//   }, 1000);
+// }
+// );
