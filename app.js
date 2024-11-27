b6b31664f968182aa493a71eb7ce0c07b2e66f52
hajratariqi <hajra.ashrafqadri@gmail.com>
2024-11-27 11:36:18 +0500

    commented code

--- app.js
+++ app.js
@@ -1,57 +1,57 @@
-const getData = (id, nextFunc) =>{
-    setTimeout(() => {
-    console.log(id);
-    if(nextFunc){
-    nextFunc()
-    }
-}, 2000)
-}  
+// const getData = (id, nextFunc) =>{
+//     setTimeout(() => {
+//     console.log(id);
+//     if(nextFunc){
+//     nextFunc()
+//     }
+// }, 2000)
+// }  
 
-// this is called callback hell
-getData(1, () => {
-    console.log('Added 1...');
-    getData(10, () => {
-    console.log('Added 10...');
-        getData(100, () => {
-    console.log('Added 100...');
-            getData(1000, () => {
-        console.log('Added 1000...');
-            })
-        })
-    })
-})
+// // this is called callback hell
+// getData(1, () => {
+//     console.log('Added 1...');
+//     getData(10, () => {
+//     console.log('Added 10...');
+//         getData(100, () => {
+//     console.log('Added 100...');
+//             getData(1000, () => {
+//         console.log('Added 1000...');
+//             })
+//         })
+//     })
+// })
 
 
-let isPizza = true;
+// let isPizza = true;
 
-const makePizza = (makeDoughtFunc, finalOrder) => {
-  if (isPizza) {
-    setTimeout(() => {
-      console.log("Cheese is here...");
+// const makePizza = (makeDoughtFunc, finalOrder) => {
+//   if (isPizza) {
+//     setTimeout(() => {
+//       console.log("Cheese is here...");
 
-      makeDoughtFunc(finalOrder);
+//       makeDoughtFunc(finalOrder);
 
-    }, 2000);
-  } else {
-    console.log("SOrry pizza is not found");
-  }
-};
+//     }, 2000);
+//   } else {
+//     console.log("SOrry pizza is not found");
+//   }
+// };
 
-makePizza(
-    (finalOrder) => {
-    setTimeout(() => {
-        console.log("pizza dought is ready");
-        finalOrder()
-    }, 1000);
-}, 
+// makePizza(
+//     (finalOrder) => {
+//     setTimeout(() => {
+//         console.log("pizza dought is ready");
+//         finalOrder()
+//     }, 1000);
+// }, 
 
-function finalOrder(){
-    setTimeout(() => {
-    console.log('pizza is ready');
-}, 4000);
-}
+// function finalOrder(){
+//     setTimeout(() => {
+//     console.log('pizza is ready');
+// }, 4000);
+// }
 
-);
+// );
 
 // const getData = (id, nextFunc) =>{
 // //     setTimeout(() => {
